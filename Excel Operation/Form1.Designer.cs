137a8279232dd22943e164dfba2a8f4658000021
Sanjeev Nair <Sanjeev.Nair@marlabs.com>
2018-03-19 19:11:50 +0530

    excel app

---
+++ Excel Operation/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Excel_Operation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadFile = new System.Windows.Forms.Button();
+            this.dgvCurrent = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCurrent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadFile
+            // 
+            this.loadFile.Location = new System.Drawing.Point(25, 25);
+            this.loadFile.Name = "loadFile";
+            this.loadFile.Size = new System.Drawing.Size(75, 23);
+            this.loadFile.TabIndex = 0;
+            this.loadFile.Text = "Load Excel";
+            this.loadFile.UseVisualStyleBackColor = true;
+            this.loadFile.Click += new System.EventHandler(this.loadFile_Click);
+            // 
+            // dgvCurrent
+            // 
+            this.dgvCurrent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCurrent.Location = new System.Drawing.Point(25, 99);
+            this.dgvCurrent.Name = "dgvCurrent";
+            this.dgvCurrent.Size = new System.Drawing.Size(520, 150);
+            this.dgvCurrent.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(504, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(504, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvCurrent);
+            this.Controls.Add(this.loadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCurrent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadFile;
+        private System.Windows.Forms.DataGridView dgvCurrent;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Excel Operation/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Excel_Operation
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void loadFile_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+       // C: \Users\sanjeev.nair\Desktop\Story8302\CopyTest8302.xlsx
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+                    Microsoft.Office.Interop.Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(file);
+                    Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Sheets[1];
+                   // Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet1 = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Sheets[2];
+
+                    Microsoft.Office.Interop.Excel.Range xlRange = xlWorkSheet.UsedRange;
+                    int totalRows = xlRange.Rows.Count;
+                    int totalColumns = xlRange.Columns.Count;
+                    //Microsoft.Office.Interop.Excel.Range xlRange1 = xlWorkSheet1.UsedRange;
+                    //int totalRows1 = xlRange1.Rows.Count;
+                    //int totalColumns1 = xlRange1.Columns.Count;
+
+                    string query, table;
+                    int ordinal;
+                    //dgvCurrent.DataSource = dt;
+                    for (int rowCount = 2; rowCount <= 265; rowCount++)
+                    {
+                        textBox1.Text = "";
+                        string update = "UPDATE GDPRDetail SET Ordinal =  {0} WHERE TableName = '{1}' AND GDPRHeadID = 3";
+                        table = Convert.ToString((xlRange.Cells[rowCount, 1] as Microsoft.Office.Interop.Excel.Range).Text);
+                        ordinal = Convert.ToInt16((xlRange.Cells[rowCount, 2] as Microsoft.Office.Interop.Excel.Range).Text);
+                        query = string.Format(update, ordinal, table.Trim());
+
+                        xlWorkSheet.Cells[rowCount, 3] = query;
+
+                        textBox1.Text = rowCount.ToString() + " Process";
+
+
+                        // table1 = Convert.ToString((xlRange1.Cells[rowCount, 1] as Microsoft.Office.Interop.Excel.Range).Text);
+
+                        //for (int row=2; row <= 266; row ++)
+                        //{
+
+                        //    table = Convert.ToString((xlRange.Cells[row, 2] as Microsoft.Office.Interop.Excel.Range).Text);
+                        //    if (table.ToString().Trim() == table1.ToString().Trim())
+                        //    {
+                        //        textBox2.Text = "";
+                        //        query = Convert.ToString((xlRange.Cells[row, 1] as Microsoft.Office.Interop.Excel.Range).Text);
+                        //        table = Convert.ToString((xlRange.Cells[row, 2] as Microsoft.Office.Interop.Excel.Range).Text);
+                        //        count = Convert.ToString((xlRange.Cells[row, 3] as Microsoft.Office.Interop.Excel.Range).Text);
+                        //        xlWorkSheet1.Cells[rowCount, 2] = table;
+                        //        xlWorkSheet1.Cells[rowCount, 5] = query;
+                        //        xlWorkSheet1.Cells[rowCount, 3] = count;
+                        //        textBox2.Text = row.ToString()+" Added";
+                        //    }
+                        //    textBox1.Text = rowCount.ToString() + " Process";
+                        //}
+
+
+                        //int startPos = query.LastIndexOf("[NS_LearnShare].") + "[NS_LearnShare].".Length;
+                        //int length = query.IndexOf("WHERE") - startPos;
+                        //string table = query.Substring(startPos, length);
+
+                        //xlWorkSheet.Cells[rowCount, 3] = query.Length;
+
+                        //xlWorkBook.Close();
+                        //xlApp.Quit();
+                        //Marshal.ReleaseComObject(xlWorkSheet);
+                        //Marshal.ReleaseComObject(xlWorkBook);
+                        //Marshal.ReleaseComObject(xlApp);
+                    }
+                    xlWorkBook.SaveAs(file, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook, null, null, null, null, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, null, null, null, null, null);
+
+                    xlWorkBook.Close();
+                    xlApp.Quit();
+
+                    Marshal.ReleaseComObject(xlWorkSheet);
+                    Marshal.ReleaseComObject(xlWorkBook);
+                    Marshal.ReleaseComObject(xlApp);
+                }
+                catch (IOException)
+                {
+                }
+            }
+           
+
+
+        }
+    }
+}
